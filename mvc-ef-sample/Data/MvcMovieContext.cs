02bfc2fa94a10934d2d17c872ffc7741002bf94e
ptc1311 <ptc1311@gmail.com>
2023-08-10 20:48:19 +0800

    first commit

---
+++ mvc-ef-sample/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using mvc_ef_sample.Models;
+
+namespace mvc_ef_sample.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<mvc_ef_sample.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ mvc-ef-sample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_ef_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
