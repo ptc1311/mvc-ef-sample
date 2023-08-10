c73f424de95b166338d41731988a2dddea6625a3
ptc1311 <ptc1311@gmail.com>
2023-08-10 21:18:07 +0800

    1. add seedData class 2. field ReleaseDate -> Release Date

--- mvc-ef-sample/Models/Movie.cs
+++ mvc-ef-sample/Models/Movie.cs
@@ -6,6 +6,8 @@ namespace mvc_ef_sample.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
