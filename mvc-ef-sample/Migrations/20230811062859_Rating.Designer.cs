5f028230a950bb2db317a56dc5d694d5e5fb0285
ptc1311 <ptc1311@gmail.com>
2023-08-11 14:50:49 +0800

    add Rating field by Entity Framework Code First way

--- mvc-ef-sample/Migrations/20230811062859_Rating.Designer.cs
+++ mvc-ef-sample/Migrations/20230811062859_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mvc_ef_sample.Data;
 
@@ -11,9 +12,11 @@ using mvc_ef_sample.Data;
 namespace mvc_ef_sample.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20230811062859_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- mvc-ef-sample/Models/SeedData.cs
+++ mvc-ef-sample/Models/SeedData.cs
@@ -22,6 +22,7 @@ namespace mvc_ef_sample.Models
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
+                    Rating = "R",
                     Price = 7.99M
                 },
                 new Movie
@@ -29,6 +30,7 @@ namespace mvc_ef_sample.Models
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "R",
                     Price = 8.99M
                 },
                 new Movie
@@ -36,6 +38,7 @@ namespace mvc_ef_sample.Models
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "R",
                     Price = 9.99M
                 },
                 new Movie
@@ -43,6 +46,7 @@ namespace mvc_ef_sample.Models
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
+                    Rating = "R",
                     Price = 3.99M
                 }
             );
